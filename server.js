a5d1beee28b1c7c44aeaea9b7a49a198b3f0ad5b
Zongheng Yang <concretevitamin@gmail.com>
2013-07-26 23:03:28 -0700

    fuck node.js.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require("http");
 var connect = require('connect');
 var express = require('express');
+
 var app = express();
 
-app.use(express.static(__dirname + "/public/"));
+app.use(express.logger());
+app.use(express.static(__dirname + "/public/html"));
+app.use(express.static(__dirname + "/public"));
 app.use(app.router);
+
+app.get('/', function(req, res){
+  res.render('index.html');
+});
 
 app.listen(process.env.PORT || 3000);
 
